b9724fc3ad3bc60371bc0272a45a12e5f43a70ab
William Gustavo Mantovani <85569623+williamgmantovani@users.noreply.github.com>
2024-11-23 11:12:25 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -57,6 +57,5 @@ def optimize():
 
 # Configuração para rodar no Render
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
